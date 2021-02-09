582f57ef8125e320378f2e99169cb28d73bbc764
Ablang-E <ablange.eric@gmail.com>
2021-02-09 03:28:24 -0600

    Added lightswitch script.

---
+++ Assets/Scripts/lightCat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightCat : MonoBehaviour
+{
+    public Light myLight;
+
+    void Start()
+    {
+        myLight = myLight.GetComponent<Light>();
+        print("12312312");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cat"))
+        {
+            print("Cat lightswitch activated");
+            myLight.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/lightHuman.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightHuman : MonoBehaviour
+{
+    public Light myLight;
+
+    void Start()
+    {
+        myLight = myLight.GetComponent<Light>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cat"))
+        {
+            print("Cat lightswitch activated");
+            myLight.enabled = true;
+        }
+    }
+}
